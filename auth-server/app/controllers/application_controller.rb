1924943a85ef698570caad01e542b8245ae076ff
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-04 12:30:26 -0400

    added a current user method to application controller

--- auth-server/app/controllers/application_controller.rb
+++ auth-server/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+        #puts session.keys
+    end
 end
