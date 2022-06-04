db86a122abe847e899b0d72f74cde19dfb90bb0d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-04 14:59:36 -0400

    configured App component to handle sign up form route

--- auth-client/src/App.js
+++ auth-client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import SignupForm from './features/users/SignUpForm';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App =() => {
   return (
-    <>
-      <p>I hope this session's hash exercise works!</p>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={< SignupForm />} />
+      </Routes>
+    </Router>
   );
 }
 
