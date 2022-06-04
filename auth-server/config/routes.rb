35548f790da58400b99796133ec8d56cc219b072
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-04 14:01:55 -0400

    added sessions custom routes to routes.rb file

--- auth-server/config/routes.rb
+++ auth-server/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/destroy'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get '/me', to: "users#show"
   get '/signup', to: "users#create"
   resources :users, only: [:index, :show, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
