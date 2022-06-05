80e7007ceed6a8443e1d8b6df2c7ea9d55aa1d8f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-05 09:55:35 -0400

    configured App component to indicate if a session user exists

--- auth-client/src/App.js
+++ auth-client/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import SignupForm from './features/users/SignUpForm';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React, {useEffect, useState} from 'react';
+//import SignupForm from './features/users/SignUpForm';
+//import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App =() => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((res) => {
+      if (res.ok) {
+        res.json().then((user) => {
+          setCurrentUser(user);
+          setIsAuthenticated(true);
+        });
+      }
+    });
+  }, []);
+
+  if (!isAuthenticated) {
+    return <div></div>;
+  }
+
   return (
-    <Router>
+    <>
+      {currentUser ? <p>yes</p> : <p>no</p>}
+    
+    {/*<Router>
       <Routes>
         <Route exact path="/" element={< SignupForm />} />
       </Routes>
-    </Router>
+  </Router>*/}
+  </>  
   );
 }
 
