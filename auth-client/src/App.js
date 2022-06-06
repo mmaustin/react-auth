6f19dd939836b31ac1b91376b26f3eeaa1ae7901
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-06 12:03:41 -0400

    added a route to login form in app component

--- auth-client/src/App.js
+++ auth-client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignupForm from './features/users/SignUpForm';
 import { FetchUser } from './features/users/FetchUser';
+import { LoginForm } from './features/users/LoginForm';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App =() => {
@@ -10,6 +11,7 @@ const App =() => {
       <Routes>
         <Route exact path="/" element={< SignupForm />} />
         <Route exact path="/fetch" element={< FetchUser />} />
+        <Route exact path="/login" element={< LoginForm />} />
       </Routes>
     </Router>  
   );
