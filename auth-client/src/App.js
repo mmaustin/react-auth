26257d61c40428d296043e670bd8f3e2e105b25e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-11 12:56:33 -0400

    commented in component code in app component

--- auth-client/src/App.js
+++ auth-client/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-/*import SignupForm from './features/users/SignUpForm';
+import SignupForm from './features/users/SignUpForm';
 import { FetchUser } from './features/users/FetchUser';
 import { LoginForm } from './features/users/LoginForm';
 import { Logout } from './features/users/Logout';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'*/
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 export const App =() => {
 
   return (
     <>
       
-    {/*<Router>
+    <Router>
       <Routes>
         <Route exact path="/" element={< SignupForm />} />
         <Route exact path="/fetch" element={< FetchUser />} />
         <Route exact path="/login" element={< LoginForm />} />
         <Route exact path="/logout" element={< Logout />} />
       </Routes>
-    </Router>*/}
+    </Router>
     </>
   );
 }
